bc5a38c01b0302b26dba882d931c39ce663cca49
mushfikur-dwip <musfikurrahmandip@gmail.com>
2025-05-07 11:46:59 +0600

    Dynamic Pricing Plan Added

--- src/components/PricingPlans.jsx
+++ src/components/PricingPlans.jsx
@@ -1,103 +1,128 @@
-import React from "react";
-import { motion } from "framer-motion";
-import { useScrollAnimation, modernFadeUp } from "@/hooks/useScrollAnimation";
-import pricingPlans from "../../src/data/PricingPlansData.json";
+import React, { useState } from 'react';
 
 const PricingPlans = () => {
-    const basicAnim = useScrollAnimation();
-    const startupAnim = useScrollAnimation();
-    const enterpriseAnim = useScrollAnimation();
-
+    const [isAnnual, setIsAnnual] = useState(true);
+        
+    const plans = [
+        {
+            name: 'Essential',
+            priceAnnual: '29',
+            priceMonthly: '35',
+            features: [
+                'Unlimited placeholder texts',
+                'Consectetur adipiscing elit',
+                'Excepteur sint occaecat cupidatat',
+                'Officia deserunt mollit anim'
+            ],
+            
+        },
+        {
+            name: 'Perform',
+            priceAnnual: '49',
+            priceMonthly: '55',
+            features: [
+                'Unlimited placeholder texts',
+                'Consectetur adipiscing elit',
+                'Excepteur sint occaecat cupidatat',
+                'Officia deserunt mollit anim',
+                'Predefined chunks as necessary'
+            ],
+            popular: true,
+        },
+        {
+            name: 'Enterprise',
+            priceAnnual: '79',
+            priceMonthly: '85',
+            features: [
+                'Unlimited placeholder texts',
+                'Consectetur adipiscing elit',
+                'Excepteur sint occaecat cupidatat',
+                'Officia deserunt mollit anim',
+                'Predefined chunks as necessary',
+                'Free from repetition'
+            ],
+        },
+    ];
+  
+ 
     return (
-        <div>
-            <div>
-                <main className="max-w-6xl mx-auto pt-10 pb-10 px-8">
-                    <div className=" mx-auto mb-14 text-center">
-                        <h1 className="text-white font-bold text-4xl lg:text-6xl lg:pr-[50px]">
-                            View Our <span className="text-[#EE2461]">Past Work</span>
-                        </h1>
-                        <p className="text-xl text-gray-500 font-medium">
-                            Choose a plan that works best for you and your team.
-                        </p>
-                    </div>
+        <div className="relative font-inter antialiased">
+            <main className="relative min-h-screen flex flex-col justify-center overflow-hidden">
+                <div className="w-full max-w-6xl mx-auto px-4 md:px-6 lg:px-8">
+                    <div>
+                        {/* Pricing toggle */}
+                        <div className="flex justify-center max-w-[14rem] m-auto mb-8 lg:mb-16">
+                            <div className="relative flex w-full p-1 bg-[#ee2461]  rounded-full">
+                                <span className="absolute inset-0 m-1 pointer-events-none" aria-hidden="true">
+                                    <span
+                                        className={`absolute inset-0 w-1/2 bg-white rounded-full shadow-sm shadow-indigo-950/10 transform transition-transform duration-150 ease-in-out ${isAnnual ? 'translate-x-0' : 'translate-x-full'}`}
+                                    ></span>
+                                </span>
+                                <button
+                                    className={`relative flex-1 text-sm font-medium h-8 rounded-full focus-visible:outline-none focus-visible:ring focus-visible:ring-white  transition-colors duration-150 ease-in-out ${isAnnual ? 'text-[#ee2461]' : 'text-white'}`}
+                                    onClick={() => setIsAnnual(true)}
+                                    aria-pressed={isAnnual}
+                                >
+                                    Yearly  <span className={`${isAnnual ? 'text-[#ee2461]' : 'text-white dark:text-slate-500'}`}>-20%</span>
+                                </button>
+                                <button
+                                    className={`relative flex-1 text-sm font-medium h-8 rounded-full focus-visible:outline-none focus-visible:ring focus-visible:ring-white  transition-colors duration-150 ease-in-out ${isAnnual ? 'text-white' : 'text-[#ee2461]'}`}
+                                    onClick={() => setIsAnnual(false)}
+                                    aria-pressed={!isAnnual}
+                                >
+                                    Monthly
+                                </button>
+                            </div>
+                        </div>
 
-                    <div className="flex flex-col justify-between items-center lg:flex-row lg:items-start">
-                        {pricingPlans.map((plan, index) => (
-                            <motion.div
-                                key={plan.id}
-                                ref={plan.title === "Basic" ? basicAnim.ref : plan.title === "Startup" ? startupAnim.ref : enterpriseAnim.ref}
-                                initial="hidden"
-                                animate={plan.title === "Basic" ? basicAnim.controls : plan.title === "Startup" ? startupAnim.controls : enterpriseAnim.controls}
-                                variants={modernFadeUp}
-                                className={`w-full flex-1 mt-8 p-8 order-${index + 1} bg-${plan.bgColor} shadow-xl rounded-3xl sm:w-96 lg:w-full`}
-                            >
-                                <div className="mb-7 pb-7 flex items-center border-b border-gray-300">
-                                    <img
-                                        src={plan.image}
-                                        alt=""
-                                        className="rounded-3xl w-20 h-20"
-                                    />
-                                    <div className="ml-5">
-                                        <span className={`block text-2xl font-semibold text-${plan.textColor}`}>{plan.title}</span>
-                                        <span>
-                                            <span className={`font-medium text-xl align-top text-${plan.textColor}`}>$&thinsp;</span>
-                                            <span className={`text-3xl font-bold text-${plan.textColor}`}>{plan.price}</span>
-                                        </span>
-                                        <span className={`font-medium text-${plan.textColor}`}> / {plan.duration}</span>
+                        {/* Pricing Plans */}
+                        <div className="max-w-sm mx-auto grid gap-6 lg:grid-cols-3 items-start lg:max-w-none">
+                            {plans.map((plan, index) => (
+                                <div key={index} className="h-full">
+                                    <div className="relative flex flex-col h-full p-6 rounded-2xl bg-white dark:bg-slate-900 border border-slate-200 dark:border-slate-900 shadow shadow-slate-950/5">
+                                        {plan.popular && (
+                                            <div className="absolute top-0 right-0 mr-6 -mt-4">
+                                                <div className="inline-flex items-center text-xs font-semibold py-1.5 px-3 bg-[#ee2461] text-white rounded-full shadow-sm shadow-slate-950/5">
+                                                    Most Popular
+                                                </div>
+                                            </div>
+                                        )}
+                                        <div className="mb-5">
+                                            <div className="text-slate-900 dark:text-slate-200 font-semibold mb-1">{plan.name}</div>
+                                            <div className="inline-flex items-baseline mb-2">
+                                                <span className="text-slate-900 dark:text-slate-200 font-bold text-3xl">$</span>
+                                                <span className="text-slate-900 dark:text-slate-200 font-bold text-4xl">{isAnnual ? plan.priceAnnual : plan.priceMonthly}</span>
+                                                <span className="text-slate-500 font-medium">/mo</span>
+                                            </div>
+                                            <div className="text-sm text-slate-500 mb-5">There are many variations available, but the majority have suffered.</div>
+                                            <a
+                                                className="w-full inline-flex justify-center whitespace-nowrap rounded-lg bg-[#ee2461] px-3.5 py-2.5 text-sm font-medium text-white shadow-sm shadow-indigo-950/10 hover:bg-indigo-600 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150"
+                                                href="#0"
+                                            >
+                                                Purchase Plan
+                                            </a>
+                                        </div>
+                                        <div className="text-slate-900 dark:text-slate-200 font-medium mb-3">Includes:</div>
+                                        <ul className="text-slate-600 dark:text-slate-400 text-sm space-y-3 grow">
+                                            {plan.features.map((feature, idx) => (
+                                                <li key={idx} className="flex items-center">
+                                                    <svg className="w-3 h-3 fill-[#ee2461] mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                                                    </svg>
+                                                    <span>{feature}</span>
+                                                </li>
+                                            ))}
+                                        </ul>
                                     </div>
                                 </div>
+                            ))}
+                        </div>
 
-                                <ul className="mb-7 font-medium text-gray-500">
-                                    {plan.features.map((feature, idx) => (
-                                        <li key={idx} className="flex text-lg mb-2">
-                                            <img src={`https://res.cloudinary.com/williamsondesign/check-${plan.textColor === "white" ? "white" : "grey"}.svg`} alt="" />
-                                            <span className="ml-3 text-black">{feature}</span>
-                                        </li>
-                                    ))}
-                                </ul>
-
-                                <a
-                                    href={plan.buttonLink}
-                                    className="flex justify-center items-center bg-indigo-600 rounded-xl py-5 px-4 text-center text-white text-xl"
-                                >
-                                    {plan.buttonText}
-                                    <img
-                                        src="https://res.cloudinary.com/williamsondesign/arrow-right.svg"
-                                        className="ml-2"
-                                        alt=""
-                                    />
-                                </a>
-                            </motion.div>
-                        ))}
                     </div>
-
-
-                </main>
-            </div>
-
-
-            <motion.div
-                ref={basicAnim.ref}
-                initial="hidden"
-                animate={basicAnim.controls}
-                variants={modernFadeUp}
-                className="-my-4"
-            >
-
-
-            </motion.div>
-            <div className="bg-[#EE2461] border border-[#ffff] rounded-3xl max-w-6xl mx-auto">
-                <div className=" h-auto py-5 px-5 flex flex-col lg:flex-row lg:items-center lg:justify-around gap-6 items-center justify-center font-semibold text-4xl text-white">
-                    <div className='flex  flex-col'>
-                        <h1 className='text-[24px] lg:text-[44px] text-center lg:text-left'>Intake and Research</h1>
-                        <p className='text-[16px] lg:text-[18px] text-center lg:text-left leading-tight font-normal'>Choose your package and fill out the intake form. We’ll gather all the details to research and script your video.</p>
-                    </div>
-                    <div><img width={150} src="https://cdn.prod.website-files.com/6641e53a27d5f5cb8a6deda4/671c669f05faeb5e1b41f017_Our%20Process%2001.webp" alt="" /></div>
                 </div>
-            </div>
+            </main>
         </div>
     );
 };
 
 export default PricingPlans;
-
